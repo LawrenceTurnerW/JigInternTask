86448adf0f490bb7412a12e827c170c8664649a2
LawrenceTurnerW <watawata.takatan@gmail.com>
2019-06-08 17:19:52 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 axios.get('https://raw.githubusercontent.com/jigjp/intern_exam/master/fukui_event.json')
     .then(response => {
-        Vue.use(VueTables.ClientTable);
 
+        Vue.use(VueTables.ClientTable);
         new Vue({
           el: "#app",
           data: {
@@ -11,48 +11,32 @@ axios.get('https://raw.githubusercontent.com/jigjp/intern_exam/master/fukui_even
               "start_date",
               "end_date",
               "event_place",
-              //これより下はchild_lowに記載
-              //"description",　
-              //"remarks",
-              //"contact",
-              //"contact_phone_number",
-              //"event_place_url",
-              //"address",
-              //"mail_address",
-              //"transportation",
+              //これら以外は子行に記載
             ],
-            data: response.data,
+            data: response.data,  //jsonデータの取得
             options: {
-              columnsDropdown: true,
-              perPageValues:[10,30,response.data.length],
-              filterByColumn: true,
-              listColums:{
-                category: [{
-                  id:"歴史",
-                  text:"歴史"
-                }]
-              },
               headings: {
                 event_name: 'イベント名',
-                description: '説明',
-                remarks: '備考',
                 category:"カテゴリ",
                 start_date:"開始日時",
                 end_date:"終了日時",
-                contact:"コンタクト",
-                contact_phone_number:"電話番号",
                 event_place:"場所",
-                event_place_url:"イベント場所URL",
-                address:"住所",
-                mail_address:"メールアドレス",
-                transportation:"アクセス",
               },
-              //sortable:false,
-              //sortable: ["event_name"],   //ソート機能
+              columnsDropdown: true,
+              uniqueKey:'description',                    //子行に渡すユニークキー
+              perPageValues:[10,30,response.data.length], //ページング機能
+              filterByColumn: true,                       //ソート機能
+              filterable: ["event_name","category"],
+              listColums: {
+                category: [{
+                }]
+              },
+              sortable:[],
               texts: {
-                filterPlaceholder: '検索する'
+                filterBy: '検索する'
               }
             }
           }
       });
-});
+
+    });
